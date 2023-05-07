49a86e9bd91b31aa3f1fcf303ec688cec3e6ff9f
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2023-05-07 10:58:18 +0200

    product update

--- app/Http/Services/NotificationService.php
+++ app/Http/Services/NotificationService.php
@@ -4,6 +4,7 @@ namespace App\Http\Services;
 
 use App\Models\PriceNotification;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
