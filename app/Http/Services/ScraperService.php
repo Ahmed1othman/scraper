e2b18dbb9e0374c32ded1a829068f20de0e8c2a3
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2023-05-12 20:27:42 +0300

    test commit for proxy

--- app/Http/Services/ScraperService.php
+++ app/Http/Services/ScraperService.php
@@ -13,46 +13,46 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class ScraperService
 {
-        public function scrape(Product $product)
-        {
+    public function scrape(Product $product)
+    {
 
-            $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
-                if ($product->platform == 'amazon') {
-                    $proxy = getProxy();
-                    $client = HttpClient::create([
-                        'proxy' => sprintf('%s:%d', $proxy->ip, $proxy->port),
-                    ]);
-                    $response = $client->request('GET', $product->url, [
-                        'headers' => [
-                            'User-Agent' => $userAgent,
-                            'verify' => false
-                        ],
-                        'timeout' => 30,
-                    ]);
+        $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
+        if ($product->platform == 'amazon') {
+            $proxy = getProxy();
+            $client = HttpClient::create([
+                'proxy' => sprintf('%s:%d', $proxy->ip, $proxy->port),
+            ]);
+            $response = $client->request('GET', $product->url, [
+                'headers' => [
+                    'User-Agent' => $userAgent,
+                    'verify' => false
+                ],
+                'timeout' => 30,
+            ]);
 
-                    if ($response->getStatusCode() == 200) {
-                        Log::info('amazon => status = 200 from scrap service');
-                        $html = $response->getContent();
-                        return $this->extractAmazon($html);
-                    }
-                }elseif ($product->platform == 'noon') {
+            if ($response->getStatusCode() == 200) {
+                Log::info('amazon => status = 200 from scrap service');
+                $html = $response->getContent();
+                return $this->extractAmazon($html);
+            }
+        } elseif ($product->platform == 'noon') {
 
-                        $proxy = getProxy();
-                        $client = HttpClient::create([
-                            'proxy' => sprintf('%s:%d', $proxy->ip, $proxy->port),
-                        ]);
-                        $response = $client->request('GET', $product->url, [
-                            'headers' => [
-                                'User-Agent' => $userAgent,
-                                'verify' => false
-                            ],
-                            'timeout' => 30,
-                        ]);
-                    if ($response->getStatusCode() == 200) {
-                        $html = $response->getContent();
-                        return $this->extractNoon($html);
-                    }
-                }
+            $proxy = getProxy();
+            $client = HttpClient::create([
+                'proxy' => sprintf('%s:%d', $proxy->ip, $proxy->port),
+            ]);
+            $response = $client->request('GET', $product->url, [
+                'headers' => [
+                    'User-Agent' => $userAgent,
+                    'verify' => false
+                ],
+                'timeout' => 30,
+            ]);
+            if ($response->getStatusCode() == 200) {
+                $html = $response->getContent();
+                return $this->extractNoon($html);
+            }
+        }
 //            } catch (TransportException $exception) {
 //                if ($proxy) {
 //                    DB::table('proxies')->where('id', $proxy->id)->update(['status' => 0]);
@@ -61,36 +61,51 @@ class ScraperService
 //            }catch(Exception $ex){
 //                return "general " . $ex->getMessage();
 //            }
-        }
 
-    function extractAmazon($crawler){
+    }
+
+    function extractAmazon($crawler)
+    {
 //        try {
 
         Log::info('from amazon scrap amazon function');
-            $matches = [];
-            preg_match('/<span class="a-price-whole">([0-9]+)<span class="a-price-decimal">\.<\/span><\/span><span class="a-price-fraction">([0-9]+)<\/span>/', $crawler, $matches);
-            $price = $matches[1] . '.' . $matches[2];
-            Log::info( 'amazon' . $price);
-        Log::info('from the end of amazon scrap');
+        $crawler = new Crawler($crawler);
+
+        $title = $crawler->filter('#productTitle')->text();
+        $price = $crawler->filter('.a-price-whole')->first()->text() . $crawler->filter('.a-price-fraction')->first()->text();
+
+        if ((!empty($title) || !empty($price)))
             return $price;
+        else
+            return null;
+    }
+
+
+
+//            $matches = [];
+//            preg_match('/<span class="a-price-whole">([0-9]+)<span class="a-price-decimal">\.<\/span><\/span><span class="a-price-fraction">([0-9]+)<\/span>/', $crawler, $matches);
+//            $price = $matches[1] . '.' . $matches[2];
+//            Log::info( 'amazon' . $price);
+//        Log::info('from the end of amazon scrap');
+//            return $price;
 //        }catch (\Exception $ex){
 //            Log::info($ex->getMessage());
 //            return null;
 //        }
-    }
-    function extractNoon($crawler){
+
+    function extractNoon($crawler)
+    {
         try {
             $matches = [];
             preg_match_all('/<div class="priceNow"[^>]*>(.*?)<\/div>/s', $crawler, $matches);
             $price = preg_replace('/[^\d\.]/', '', $matches[1][0]);
-            Log::info( 'noon' . $price);
+            Log::info('noon' . $price);
             return $price;
-        }catch (\Exception $ex){
+        } catch (\Exception $ex) {
             Log::info($ex->getMessage());
             return null;
         }
 
     }
-
-
 }
+
